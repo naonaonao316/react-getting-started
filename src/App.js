95ad3dba8df0710fb710bd9bdddc4129e67340b8
Nao Takegawa <nao.takegawa316@gmail.com>
2018-03-25 19:09:06 +0900

    commit basic sources

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Tutorial1 from './Tutorial1';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Tutorial1 />
+      </div>
+    );
+  }
+}
+
+export default App;
